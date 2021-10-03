cb7855b09f0852307d6104bbdb2dc3fa81717baf
Dorian Zedler <dorian@itsblue.de>
2021-10-03 19:41:08 +0200

    First working version

---
+++ Dockerfile
@@ -0,0 +1,56 @@
+FROM ghcr.io/linuxserver/baseimage-alpine-nginx:3.14
+
+# set version label
+ARG BUILD_DATE
+ARG VERSION
+ARG MRBS_RELEASE
+LABEL build_version="Version:- ${VERSION} Build-date:- ${BUILD_DATE}"
+LABEL maintainer="Dorian Zedler <mail@dorian.im>"
+
+# package versions
+ARG MRBS_RELEASE
+
+RUN \
+  echo "**** install packages ****" && \
+  apk update && \
+  apk add --no-cache  \
+    curl \
+    mysql-client \
+    php7-ctype \
+    php7-curl \
+    php7-dom \
+    php7-gd \
+    php7-ldap \
+    php7-mbstring \
+    php7-memcached \
+    php7-mysqlnd \
+    php7-openssl \
+    php7-pdo_mysql \
+    php7-phar \
+    php7-simplexml \
+    php7-tokenizer \
+    tar && \
+  echo "**** configure php-fpm ****" && \
+  sed -i 's/;clear_env = no/clear_env = no/g' /etc/php7/php-fpm.d/www.conf && \
+  echo "env[PATH] = /usr/local/bin:/usr/bin:/bin" >> /etc/php7/php-fpm.conf && \
+  echo "**** fetch mrbs ****" && \
+  rm -rf /var/www/html && \
+  mkdir -p\
+    /var/www/html && \
+  if [ -z ${MRBS_RELEASE+x} ]; then \
+    MRBS_RELEASE=$(curl -sX GET "https://api.github.com/repos/meeting-room-booking-system/mrbs-code/releases/latest" \
+    | awk '/tag_name/{print $4;exit}' FS='[""]'); \
+  fi && \
+  curl -o \
+  /tmp/mrbs.tar.gz -L \
+    "https://github.com/meeting-room-booking-system/mrbs-code/archive/${MRBS_RELEASE}.tar.gz" && \
+  echo "**** extract only folder 'web' ****" && \
+  tar -C /var/www/html --strip-components=2 -zxvf /tmp/mrbs.tar.gz $(tar --exclude="*/*" -tf /tmp/mrbs.tar.gz)web && \
+  echo "**** cleanup ****" && \
+  rm -rf \
+    /tmp/*
+
+COPY root/ /
+
+VOLUME /config
+EXPOSE 80
---
+++ root/var/www/html/config.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/*******************
+ * Database settings
+ ******************/
+// Which database system: "pgsql"=PostgreSQL, "mysql"=MySQL
+$dbsys = "mysql";
+// Hostname of database server. For pgsql, can use "" instead of localhost
+// to use Unix Domain Sockets instead of TCP/IP. For mysql "localhost"
+// tells the system to use Unix Domain Sockets, and $db_port will be ignored;
+// if you want to force TCP connection you can use "127.0.0.1".
+$db_host = getenv("DB_HOST");
+// If you need to use a non standard port for the database connection you
+// can uncomment the following line and specify the port number
+// $db_port = 1234;
+// Database name:
+$db_database = getenv("DB_DATABASE");
+// Schema name.  This only applies to PostgreSQL and is only necessary if you have more
+// than one schema in your database and also you are using the same MRBS table names in
+// multiple schemas.
+//$db_schema = "public";
+// Database login user name:
+$db_login = getenv("DB_USER");
+// Database login password:
+$db_password = getenv("DB_PASS");
+// Prefix for table names.  This will allow multiple installations where only
+// one database is available
+$db_tbl_prefix = "mrbs_";
+// Set $db_persist to TRUE to use PHP persistent (pooled) database connections.  Note
+// that persistent connections are not recommended unless your system suffers significant
+// performance problems without them.   They can cause problems with transactions and
+// locks (see http://php.net/manual/en/features.persistent-connections.php) and although
+// MRBS tries to avoid those problems, it is generally better not to use persistent
+// connections if you can.
+$db_persist = FALSE;
+
+require "/config/www/config.inc.php";
